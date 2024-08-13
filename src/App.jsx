42d5e5ff20d8036ebc8e3c99180b6edb35562079
Timilehin Amolegbe <timilehinamolegbe1@gmail.com>
2024-08-13 10:55:01 +0100

    Use state and Use Effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+/*
+///////////////////////////////
+// component and prop
 // For multiple elements
 const Person = (props) => {
   return (
@@ -28,11 +31,41 @@ const Person2 = ({ name, lastName, age }) => {
 const App = () => {
   return (
     <div className="App">
-      <Person name={"Timmy"} lastName={"Feds"} age={30} />
-      <Person2 name={"Micheal"} lastName={"Dave"} age={29} />
-      <Person />
-      <Person />
-      <Person />
+      <Person 
+      name="Timmy" 
+      lastName="Feds" 
+      age={15 + 15} 
+      />
+      
+      <Person2 
+      name="Micheal" 
+      lastName="Dave" 
+      age={29} 
+      />
+    </div>
+  );
+};
+*/
+
+/////////////////////////////////
+//React State and Effect
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    alert(`You've Changed the Counter to ${counter}`);
+  }, [counter]);
+
+  return (
+    <div className="App">
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+        -
+      </button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+        +
+      </button>
     </div>
   );
 };

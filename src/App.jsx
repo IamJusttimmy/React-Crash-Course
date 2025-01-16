5da55099c009817996f206cec2ff97decba750c3
Timilehin Amolegbe <timilehinamolegbe1@gmail.com>
2025-01-16 19:32:48 +0100

    APIs

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg";
 
-const API_URL = "http://www.omdbapi.com?apikey=5020d60a";
+const API_URL = "https://www.omdbapi.com?apikey=5020d60a";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
